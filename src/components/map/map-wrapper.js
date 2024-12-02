2f3a6749d1936e37703dc8615e8ea13e0efb9ded
chamupathi-mendis <chamupathi.mendis.cs@gmail.com>
2024-12-02 17:51:27 +0000

    remove defulat test add default zoom

--- src/components/map/map-wrapper.js
+++ src/components/map/map-wrapper.js
@@ -1,11 +1,12 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const defaultStyles = { width: '100%', height: '100%' }
+const DEFAULTS_ZOOM = 15
 
 const MapWrapper = ({
     locations,
     center,
-    zoom = 15,
+    zoom = DEFAULTS_ZOOM,
     scrollWheelZoom = false,
     styles = defaultStyles
 }) => <MapContainer
