13e4ff1a5c32354536456da060d41d968a2cf906
chamupathi-mendis <chamupathi.mendis.cs@gmail.com>
2024-12-02 17:02:14 +0000

    comitting just after the call

---
+++ src/components/resturants-map.js
@@ -0,0 +1,43 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useGeoLocationData } from "../hooks/useGeoLocationData";
+
+
+
+// // Example Usage
+// const latitude = 37.7749; // Example Latitude (San Francisco)
+// const longitude = -122.4194; // Example Longitude (San Francisco)
+// const radius = 1000; // Radius in meters
+
+// fetchOpenStreetMapData(latitude, longitude, radius)
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+
+const center = [
+    51.5039555, -0.0175315
+]
+
+const ResturantsMap = () => {
+
+    const locations = useGeoLocationData(center[0], center[1], 1000);
+
+    return <div style={{ width: '100%', height: '80vh' }}>
+        <MapContainer center={center}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ width: '100%', height: '100%' }}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={center}>
+                <Popup>
+                    Comply cube HQ
+                </Popup>
+            </Marker>
+        </MapContainer>
+
+    </div>
+}
+export default ResturantsMap;
---
+++ src/hooks/useGeoLocationData.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+
+export const useGeoLocationData = (lat, lon, radius) => {
+
+    // console.log(lat, lon, radius);
+
+    const [locations, setLocations] = useState([]);
+
+    useEffect(() => {
+        const getData = async (lat, lon, radius) => {
+    console.log(lat, lon, radius);
+
+            // const overpassApiUrl = `https://overpass-api.de/api/interpreter`;
+
+        
+            // Overpass API query to find points of interest (POIs) around the given location
+        
+            const query = `https://overpass-api.de/api/interpreter?data=[out:json];node(around:${radius},${lat},${lon})[amenity=restaurant];out;`
+            try {
+                const response = await fetch(query);
+        
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+        
+                const data = await response.json();
+
+                // return data; // Contains the geographic features around the point
+                console.log('data',data)
+
+                setLocations()
+            } catch (error) {
+                console.error("Error fetching data from OpenStreetMap:", error);
+                return null;
+            }
+        }
+
+        getData(lat, lon, radius);
+    }, [lat, lon, radius])
+
+    return locations;
+}
